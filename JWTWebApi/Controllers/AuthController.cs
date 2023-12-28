5c539e75e08a443caf3f0979e2f355598e3038ab
Puja-Negi <Puja.negi@truenary.com>
2023-12-28 12:33:50 +0545

    Add project files.

---
+++ JWTWebApi/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using JWTWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request) 
+        {
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.Password); //HashPassword function takes plain text password 'request.Password' as an input parameter and computes its secure hash value
+
+            user.Username = request.Username;   
+            user.PasswordHash = passwordHash;
+
+            return  Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+           if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+           if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            //SymmetricSecurityKey obj represents a cryptographic key for generating and validating tokens
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); //Creates SigningCredentials to sign the token using the secret key and a specified signature algorithm
+
+
+            //Create JWT token
+            var token = new JwtSecurityToken( 
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
